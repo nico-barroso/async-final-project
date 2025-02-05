689293d9c82732d97eddc776b2580891bca7f5c6
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2025-02-05 00:37:00 +0000

    Reverted

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import "./style.css";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Home } from "./public/pages/Home/Home";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 const webBuilder = () => {
   NavBar();
   Home();
-  SpeedInsights();
+
 };
 
 webBuilder();
