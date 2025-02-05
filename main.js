c2f85b41c419b890975dbd33d697c9a710800719
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2025-02-05 00:34:37 +0000

    Vercel changes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Home } from "./public/pages/Home/Home";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const webBuilder = () => {
   NavBar();
@@ -8,3 +9,17 @@ const webBuilder = () => {
 };
 
 webBuilder();
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
