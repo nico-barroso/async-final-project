d7b5ac99c02723a22f5fa3d74fe1dadc7eb8a994
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2025-02-05 00:25:11 +0000

    Added API changes

--- utils/UnsplashAPI/UnsplashAPI.js
+++ utils/UnsplashAPI/UnsplashAPI.js
@@ -4,11 +4,13 @@
  * @param {*} page Selecciona la página que quiere que devuelva la imágen con un máximo de 30 imágenes por página (Int)
  * @returns Resultados de la búsqueda en la API de Unsplash.
  */
+const API_KEY = "0122kjLl1tfPL3iOFvbAQwVDoRV2vgA72nrO5ebFJAc";
 export const getPhotos = async (query, page) => {
   try {
     const response = await fetch(
       `https://api.unsplash.com/search/photos?page=${page}&query=${query}&per_page=30&client_id=${
-        import.meta.env.VITE_CLIENT_ID
+        // import.meta.env.VITE_CLIENT_ID
+        API_KEY
       }`
     );
     const results = await response.json();
